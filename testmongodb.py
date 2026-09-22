ebb0f6c1a08cb785fb14b11514c251b8c8c81151
Vipul Mapara <vipulmapara1115@gmail.com>
2026-09-22 17:22:22 -0500

    some changes in setup file

--- testmongodb.py
+++ testmongodb.py
@@ -1,10 +1,10 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
-from pymongo.mongo_client import MongoClient
-
-uri = "mongodb+srv://krishaitechnologies:<@password>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://vipulmapara1115_db_user:seemad810@cluster0.e0lqdpx.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri)
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
